3f6108f13afbb8598ffa7bd8a385c3b7e3a1e7fa
mozibrishu <mozib.diu@gmail.com>
2021-03-11 11:46:59 +0600

    Skeleton Structure Ready

--- src/components/TeamDetails/TeamDetails.js
+++ src/components/TeamDetails/TeamDetails.js
@@ -1,29 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-
-import './TeamDetails.css'
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import "./TeamDetails.css";
+import maleImage from "../../images/male.png";
+import femaleImage from "../../images/female.png";
 
 const TeamDetails = () => {
-    const [team,setTeam] = useState({});
-    const {teamId} = useParams();
+  const [team, setTeam] = useState({});
+  const { teamId } = useParams();
 
-    useEffect(()=>{
-        const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${teamId}`;
-        fetch(url)
-        .then(res => res.json())
-        .then(data => setTeam(data.teams[0]))
-    },[teamId])
-    const {strTeamBadge,strCountry, strDescriptionEN,strFacebook,strTwitter,strYoutube,strInstagram,intFormedYear} = team;
-    return (
-        <div>
-            {/* <Banner badge={strTeamBadge}></Banner> */}
-            <div className="banner background-opacity">
-                <div className="title"><img className="teamBadge" src={strTeamBadge} alt=""/></div>
-            </div>
-            <h1>Team Details:{teamId}</h1>
-            <h2>{strCountry}</h2>
+  useEffect(() => {
+    const url = `https://www.thesportsdb.com/api/v1/json/1/lookupteam.php?id=${teamId}`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setTeam(data.teams[0]));
+  }, [teamId]);
+
+  const {
+    strTeamBadge,
+    strCountry,
+    strGender,
+    strTeam,
+    strSport,
+    strDescriptionEN,
+    strFacebook,
+    strTwitter,
+    strYoutube,
+    strInstagram,
+    intFormedYear,
+  } = team;
+
+  let imageSrc = "";
+  if (strGender === "Male") {
+    imageSrc = maleImage;
+  } else {
+    imageSrc = femaleImage;
+  }
+
+  return (
+    <div>
+
+      <div className="banner background-opacity">
+        <div className="title">
+          <img className="teamBadge" src={strTeamBadge} alt="" />
         </div>
-    );
+      </div>
+
+      <div className="details-card row row-cols-1  row-cols-md-2">
+        <div className="details">
+          <h2>{strTeam}</h2>
+          <p>Founded: {intFormedYear}</p>
+          <p>Country: {strCountry}</p>
+          <p>Sport Type: {strSport}</p>
+          <p>Gender: {strGender}</p>
+        </div>
+
+        <div className="team-image">
+          <img className="gender-pic" src={imageSrc} alt="" />
+        </div>
+      </div>
+
+      <div className="detail-description">
+        <p>{strDescriptionEN}</p>
+      </div>
+    </div>
+  );
 };
 
 export default TeamDetails;
