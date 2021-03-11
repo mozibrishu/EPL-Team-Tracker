874e25cfa4e8625d9c16cf5f09f5c6696937c904
mozibrishu <mozib.diu@gmail.com>
2021-03-11 20:27:09 +0600

    Main Part Complete

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # EPL(English Premier League) Team Tracker
-Type of team, team's badge have been shown on Home page.
-In details page, team's banner, foundation year, origin country and short description have been shown.
-thesportsdb API has been used to fetch details about Teams.
+Type of team, team's badge have been shown on Home page.\
+In details page, team's banner, foundation year, origin country and short description have been shown.\
+thesportsdb API has been used to fetch details about Teams.\
 We have used font awesome icons and react-router-dom for routing.
 
 ## Live Site Link
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,7 @@ const Home = () => {
 
   useEffect(() => {
     const url =
-      "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?s=Soccer&c=England";
+      "https://www.thesportsdb.com/api/v1/json/1/search_all_teams.php?l=English%20Premier%20League";
     fetch(url)
       .then((res) => res.json())
       .then((data) => setTeams(data.teams));
@@ -15,7 +15,7 @@ const Home = () => {
   return (
     <div>
       <div className="banner">
-        <h1 className="title">Follow Team</h1>
+        <h1 className="title">EPL Team Tracker</h1>
       </div>
 
       <div className="teams-container">
